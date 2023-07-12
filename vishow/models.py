fe7c758deff8ca02681a54d4a2aa053f211891ed
liguanghe <liguanghe218317@gmail.com>
2023-07-12 08:55:43 +0800

    api获取实时股票信息

--- vishow/models.py
+++ vishow/models.py
@@ -2,27 +2,37 @@ import requests
 from django.db import models
 import efinance as ef
 import pandas as pd
+from django.utils import timezone
+import datetime
+
+
 
 class Stock(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)  # 设置默认值为0.00
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)
     market_cap = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0.00)  # 添加流通市值字段
+    date = models.DateField(default=datetime.date.today)# 添加日期字段
+    time = models.TimeField(default=timezone.now)  # 添加时间字段
 
     # 其他代码...
 
     def update_price(self):
         try:
-            # 获取实时股票行情数据
-            quotes = ef.stock.get_realtime_quotes()
+            # 根据code来获取实时股票行情数据
+            quotes = ef.stock.get_latest_quote(self.code)  # 修正获取实时股票行情数据的参数
 
             # 筛选所需字段
-            filtered_quotes = quotes[['股票名称', '流通市值', '最新价']]  # 添加流通市值字段
+            filtered_quotes = quotes[['代码', '名称', '流通市值', '最新价']]  # 添加流通市值字段
+
+            # 添加日期时间列
+            filtered_quotes['日期'] = pd.Timestamp.now().date()
+            filtered_quotes['时间'] = pd.Timestamp.now().time()
 
             # 打印结果
             print(filtered_quotes)
-
-            self.name = filtered_quotes.iloc[0]['股票名称']
+            self.code = filtered_quotes.iloc[0]['代码']
+            self.name = filtered_quotes.iloc[0]['名称']
             self.market_cap = float(filtered_quotes.iloc[0]['流通市值'])  # 更新流通市值字段
             self.price = float(filtered_quotes.iloc[0]['最新价'])
             self.save()
@@ -30,5 +40,4 @@ class Stock(models.Model):
             # 处理异常情况，例如输出错误信息或进行其他适当的处理
             print(f"Failed to update stock price: {e}")
 
-    # 其他代码...
 
--- vishow/views.py
+++ vishow/views.py
@@ -4,14 +4,11 @@ from .models import Stock
 def home(request):
     if request.method == 'POST':
         code = request.POST.get('stock_code')
-        stock, created = Stock.objects.get_or_create(code=code)
-        if created:
-            stock.update_price()
+        stock = Stock.objects.create(code=code)  # 创建新的股票对象
+        stock.update_price()  # 更新股票数据并创建新的记录
 
     stocks = Stock.objects.all()
     for stock in stocks:
-        stock.update_price()
+        stock.update_price()  # 更新股票数据并创建新的记录
 
     return render(request, 'home.html', {'stocks': stocks})
-
-
