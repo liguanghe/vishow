8a96ec3008589791962dcf2cd89ae6849318e835
liguanghe <liguanghe218317@gmail.com>
2023-07-14 14:39:50 +0800

    添加了文本框,但有数据问题. 不算成功. 接下来让gpt重新把结构梳理一下,进行改动.

--- vishow/views.py
+++ vishow/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Stock
 
-def home(request):
-    if request.method == 'POST':
-        code = request.POST.get('stock_code')
-        stock = Stock.objects.create(code=code)  # 创建新的股票对象
-        stock.update_price()  # 更新股票数据并创建新的记录
-        stock = Stock.objects.get(id=stock.id)  # 重新从数据库中获取股票对象
+def convert_to_trillion(number):
+    if number is not None:
+        return round(number / 10**12, 4)
+    else:
+        return None
 
+def home(request):
     stocks = Stock.objects.all()
+
+    # Convert the circulation market value, buy price and sell price to the form of trillion
     for stock in stocks:
-        assert stock.id is not None, f"Stock object has no id: {stock}"
-        stock.update_price()  # 更新股票数据并创建新的记录
-        stock = Stock.objects.get(id=stock.id)  # 重新从数据库中获取股票对象
+        stock.market_cap = convert_to_trillion(stock.market_cap)
+        stock.buy_price = convert_to_trillion(stock.buy_price)
+        stock.sell_price = convert_to_trillion(stock.sell_price)
 
     return render(request, 'home.html', {'stocks': stocks})
 
-
-
 def update_plan_price(request, stock_id):
-    stock = get_object_or_404(Stock, pk=stock_id)
-    if request.method == 'POST':
-        new_buy_price = request.POST.get('buy_price')
-        new_sell_price = request.POST.get('sell_price')
-        if new_buy_price:
-            stock.buy_price = new_buy_price
-        if new_sell_price:
-            stock.sell_price = new_sell_price
+    stock = get_object_or_404(Stock, id=stock_id)
+    if request.method == "POST":
+        buy_price = request.POST.get('buy_price')
+        sell_price = request.POST.get('sell_price')
+        
+        # Convert the buy price and sell price to the form of trillion
+        buy_price = convert_to_trillion(float(buy_price))
+        sell_price = convert_to_trillion(float(sell_price))
+
+        stock.buy_price = buy_price
+        stock.sell_price = sell_price
         stock.save()
-    return redirect('home')
+
+        return redirect('home')
+
+    return render(request, 'home.html', {'stock': stock})
